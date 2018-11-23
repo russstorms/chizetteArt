9b8013391471dfda4e2e0ab1c8c39f404769fbf5
Russ Storms <rstorms90@gmail.com>
2018-11-23 15:12:04 -0700

    Catching errors implemented, needs middleware and validation

--- routes/chizetteart.js
+++ routes/chizetteart.js
@@ -1,24 +1,87 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
+
 // READ ALL records for this table
 router.get('/', (req, res, next) => {
-  res.send('ALL THE ART!')
-})
+    knex('chizetteart')
+      .then((rows) => {
+        res.json(rows)
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
+
 // READ ONE record for this table
 router.get('/:id', (req, res, next) => {
-  res.send('GIMME JUST ONE!')
-})
+    knex('chizetteart')
+      .where('id',req.params.id)
+      .then((rows) => {
+        res.json(rows)
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
+
 // CREATE ONE record for this table
 router.post('/', (req, res, next) => {
-  res.send('POST THIS MASTAPIECE!')
-})
+    knex('chizetteart')
+      .insert({
+        "Title": req.body.Title,
+        "Year": req.body.Year,
+        "Description": req.body.Description
+      })
+      .returning('*')
+      .then((data) => {
+        res.json(data[0])
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
+
 // UPDATE ONE record for this table
 router.put('/:id', (req, res, next) => {
-  res.send('EDITED THAT MASTAPIECE!')
+  knex('chizetteart')
+  .where('id', req.params.id)
+  .then((data) => {
+    knex('chizetteart')
+    .where('id', req.params.id)
+    .limit(1)
+    .update({
+      "Title": req.body.Title,
+      "Year": req.body.Year,
+      "Description": req.body.Description
+    })
+    .returning('*')
+    .then((data) => {
+      res.json(data[0])
+    })
+  })
+  .catch((err) => {
+    next(err)
+  })
 })
+
 // DELETE ONE record for this table
-router.delete('/:id', (req, res, next) => {
-  res.send('THIS ONE IS YUCK')
-})
+router.delete('/:id', function(req, res, next) {
+    knex('chizetteart')
+      .where('id', req.params.id)
+      .first()
+      .then((row) => {
+        if(!row) return next()
+        knex('chizetteart')
+          .del()
+          .where('id', req.params.id)
+          .then(() => {
+            res.send(`ID ${req.params.id} Deleted`)
+          })
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
+
 module.exports = router
