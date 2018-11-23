f4b14c90cd4bc4b78df1cabe0d2a883cb2480a0a
Russ Storms <rstorms90@gmail.com>
2018-11-23 14:38:28 -0700

    created db

---
+++ public/javascripts/knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+      client: 'pg',
+      connection: 'postgres://localhost/dbname-dev'
+    },
+    test: {},
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL
+    }
+  }
