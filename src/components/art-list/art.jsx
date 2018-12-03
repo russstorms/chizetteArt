cbbf05488048de09403fababa168bfb3ee9bd27d
Russ Storms <rstorms90@gmail.com>
2018-12-02 17:32:10 -0700

    art showing up 🎨

--- src/components/art-list/art.jsx
+++ src/components/art-list/art.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Art = (props) => {
     return (
         <div className="art">
+            <hr />
             <div><b>Title:</b> <i>{props.art.title}</i></div>
-            <div><i> </i></div>
             <br />
-            <div><b>Year:</b> </div>
+            <div><b>Year:</b> {props.art.year}</div>
             <br />
-            <div><b>Medium:</b> </div>
+            <div><b>Medium:</b> {props.art.medium}</div>
             <br />
-            <div><b>Description:</b> </div>
+            <div><b>Description:</b> {props.art.description}</div>
             <br />
-            <div><b>Poster:</b> </div>
+            <div><b>Poster:</b> {props.art.poster}</div>
             <br />
         </div>
     )
