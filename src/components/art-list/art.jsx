52e3a3d3ab60867777783a3995be1c89204cfda0
Russ Storms <rstorms90@gmail.com>
2018-12-02 20:05:46 -0700

    reindenting

--- src/components/art-list/art.jsx
+++ src/components/art-list/art.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 
 const Art = (props) => {
-    return (
-        <div>
-            <hr />
-            <div><img src={props.art.poster} alt='https://placekitten.com/200/300'></img></div>
-            <br />
-            <div><b>Title:</b> <i>{props.art.title}</i></div>
-            <br />
-            <div><b>Year:</b> {props.art.year}</div>
-            <br />
-            <div><b>Medium:</b> {props.art.medium}</div>
-            <br />
-            <div>{props.art.description}</div>
-            <br />
-        </div>
+  return (
+    <div>
+      <hr />
+        <div><img src={props.art.poster} alt='https://placekitten.com/200/300'></img></div>
+        <br />
+        <div><b>Title:</b> <i>{props.art.title}</i></div>
+        <br />
+        <div><b>Year:</b> {props.art.year}</div>
+        <br />
+        <div><b>Medium:</b> {props.art.medium}</div>
+        <br />
+        <div>{props.art.description}</div>
+        <br />
+    </div>
     )
 }
 
--- src/components/art-list/artList.jsx
+++ src/components/art-list/artList.jsx
@@ -3,21 +3,21 @@ import Art from './art'
 import './art.css'
 
 const ArtList = (props) => {
-    return (
-        <div className="container">
-            <div className="row">
-                <div>
-                    <h1 className="artList col s6">Art List</h1>
-                    <ul>
-                        {props.artList.map(
-                            (art, idx) => {
-                                return <Art key={idx} art={art} />
-                            }
-                        )}
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div>
+          <h1 className="artList col s6">Art List</h1>
+            <ul>
+              {props.artList.map(
+                (art, idx) => {
+                  return <Art key={idx} art={art} />
+                }
+              )}
+            </ul>
         </div>
+      </div>
+    </div>
     )
 }
 
