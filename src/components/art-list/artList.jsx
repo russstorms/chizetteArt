7d7bc4c259838159911a47ff5a4108d1dfe469b7
Russ Storms <rstorms90@gmail.com>
2018-12-03 09:37:04 -0700

    got materialize to work🎨

--- src/components/art-list/artList.jsx
+++ src/components/art-list/artList.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import Art from './art'
-import { Row, Col } from 'react-materialize'
 import './art.css'
+import { Container, Row, Col } from 'react-materialize'
 
 const ArtList = (props) => {
   return (
-    <Row>
-      <Col s={6}>
-        <h1 className="artList">Art List</h1>
-        <ul>
-          {props.artList.map(
-            (art, idx) => {
-              return <Art key={idx} art={art} />
-            }
-          )}
-        </ul>
-      </Col>
-    </Row>
+    <Container>
+      <Row>
+        <Col s={6}>
+          <h1 className="artList">Art List</h1>
+          <ul>
+            {props.artList.map(
+              (art, idx) => {
+                return <Art key={idx} art={art} />
+              }
+            )}
+          </ul>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
