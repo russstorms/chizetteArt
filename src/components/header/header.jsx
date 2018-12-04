ab33b2e68006f005695b997b2333d1b7e16e38fd
Russ Storms <rstorms90@gmail.com>
2018-12-04 12:45:46 -0700

    blah

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import './header.css'
 
 export default class header extends Component {
-    render() {
-        return (
-            <div className="nav">
-                <h1 className="chizetteArt">chizetteArt</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="nav">
+        <h1 className="chizetteArt">chizetteArt</h1>
+      </div>
+    )
+  }
 }
