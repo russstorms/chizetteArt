011c28ae53111134ff018b01e973860a3c5fe93f
Russ Storms <rstorms90@gmail.com>
2018-12-04 15:41:38 -0700

    working on login functionality

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -9,7 +9,8 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      artList: []
+      artList: [],
+      login: false
     }
   }
 
--- src/components/login/loginForm.jsx
+++ src/components/login/loginForm.jsx
@@ -1,26 +1,50 @@
 import React from 'react'
 import './loginForm.css'
-import { Row, Col } from 'react-materialize'
+// import { Row, Col } from 'react-materialize'
 
-const Login = (props) => {
-	return (
-		<Row className="loginForm">
-			<Col className="s6 push-s3">
-				<h2></h2>
-				<form>
-					<label>
-					  Email:
-					  <input type="text" name="email" />
-					</label>
-					<label>
-					  Password:
-					  <input type="text" name="email" />
-					</label>
-					<input type="submit" value="Submit" />
-				</form>
-			</Col>
-		</Row>
-	)
+const handleChange = (e) => {
+	this.setState({ value: e.target.value })
 }
 
-export default Login
+const handleSubmit = ({ handleChange }) => {
+	alert('a name was submitted: ' + this.state.value)
+	e.preventDefault()
+}
+
+render() {
+	
+}
+export default LoginForm
+
+// const onLogin = () => {
+// 	const onSubmit = (e) => {
+// 		e.preventDefault()
+// 		console.log(e.target.value)
+// 		// checkLogin({
+// 		// 	username: e.target.subject.value,
+// 		// 	pass: e.target.
+// 		// })
+// 	}
+// }
+// const onSubmit = (e) => {
+// 	e.preventDefault()
+// 	console.log(e.target)
+// }
+
+// return (
+// 	<Row className="loginForm">
+// 		<Col className="s6 push-s3">
+// 			<form>
+// 				<label htmlFor="icon_prefix">
+// 				<i className="material-icons prefix"></i>
+// 				<input id="icon_prefix" autoComplete="off" placeholder="Username" type="text" name="username" value={this.state.value} />
+// 				</label>
+// 				<label htmlFor="icon_lock">
+// 				<i className="material-icons prefix"></i>
+// 				<input id="icon_lock" autoComplete="off" placeholder="Password" type="password" name="password" value={this.state.value} />
+// 				</label>
+// 				<button className="goButton">GO</button>
+// 			</form>
+// 		</Col>
+// 	</Row>
+// )
