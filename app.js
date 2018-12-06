47786b0791cfdf7fc20cfee4f73729a8e456ba73
Russ Storms <rstorms90@gmail.com>
2018-12-06 10:47:51 -0700

    env variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors')
 const express = require('express')
+const basicAuth = require('express-basic-auth')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
@@ -21,6 +22,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(basicAuth({
+  users: { 'admin': 'supersecret' }
+}))
 
 //// CORS HEADERS \\\\
 app.use((req, res, next) => {
