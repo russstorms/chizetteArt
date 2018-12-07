75094055a509575f163eb5cb9a75e52b81ad279f
Russ Storms <rstorms90@gmail.com>
2018-12-06 18:12:40 -0700

    changes to auth 🔐

--- auth/authServices.js
+++ auth/authServices.js
@@ -1,38 +1,38 @@
-// const knex = require('../knex')
-// const bcrypt = require('bcryptjs')
+const knex = require('../knex')
+const bcrypt = require('bcryptjs')
 
-// class adminService {
+class adminService {
 
-//   getAdminName() {
-//     return knex('admin')
-//       .select('username')
-//   }
+  getAdminName() {
+    return knex('admin')
+      .select('username')
+  }
 
-//   getSecretFor(id) {
-//     return knex('admin')
-//       .select('secret')
-//       .first()
-//       .where({id})
-//   }
+  getSecretFor(id) {
+    return knex('admin')
+      .select('secret')
+      .first()
+      .where({id})
+  }
 
-//   getUser(username) {
-//     return knex('admin')
-//       .first()
-//       .where({username})
-//   }
+  getUser(username) {
+    return knex('admin')
+      .first()
+      .where({username})
+  }
 
-//   tryLoginAdmin(username, password) {
-//     return knex('admin')
-//       .select('password')
-//       .first()
-//       .where({username})
-//       .then(queryResult => {
-//         if (!queryResult) return false
-//         return bcrypt.compare(password, queryResult.password).then(function(res) {
-//           return res
-//         })
-//       })
-//   }
-// }
+  tryLoginAdmin(username, password) {
+    return knex('admin')
+      .select('password')
+      .first()
+      .where({username})
+      .then(queryResult => {
+        if (!queryResult) return false
+        return bcrypt.compare(password, queryResult.password).then(function(res) {
+          return res
+        })
+      })
+  }
+}
 
-// module.exports = adminService
+module.exports = adminService
