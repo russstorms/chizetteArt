6b9b0536564cb8e9e231c1132ab2f1c83214fdca
Russ Storms <rstorms90@gmail.com>
2018-12-08 14:12:32 -0700

    i hate auth

--- migrations/20181206163112_admin.js
+++ migrations/20181206163112_admin.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('admin', table => {
       table.increments('id').primary()
       table.string('username').notNull()
-      table.string('secret').notNull()
+      table.string('token').notNull()
       table.string('password').notNull()
       table.timestamps(true, true)
     })
