59f867b14ffe3279ffe7c679cb98ce302cf5607e
Russ Storms <rstorms90@gmail.com>
2018-12-20 21:53:56 -0700

    dealt with backend signup

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -5,11 +5,11 @@ const requireSignIn = passport.authenticate('local', {session: false})
 const router = express.Router()
 
 //// SIGNIN AUTH \\\\
-router.get('/sign-up', (req, res) => {
-  res.render('authentication/sign-up')
-})
+// router.get('/sign-up', (req, res) => {
+//   res.render('authentication/sign-up')
+// })
 
-router.post('/sign-up', authentication.signup)
+// router.post('/sign-up', authentication.signup)
 
 router.get('/sign-in', (req, res) => {
   res.render('authentication/sign-in')
