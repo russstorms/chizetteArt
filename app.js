1f81628481ad2a7435451a04a293b587c5861aae
Russ Storms <rstorms90@gmail.com>
2018-12-23 15:04:57 -0700

    deploying to heroku

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //// CORS HEADERS \\\\
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Headers', '*')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE, PUT')
   res.header('Referrer-Policy', 'no-referrer')
   if (req.method === 'OPTIONS') {
