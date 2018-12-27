51b3c8a364a928c361376902f35fbfa4d41a8737
Russ Storms <rstorms90@gmail.com>
2018-12-27 13:21:45 -0700

    worked on migrations

--- migrations/20181123144038_chizetteart.js
+++ migrations/20181123144038_chizetteart.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
       table.string('title').notNullable().defaultTo('')
       table.integer('year').notNullable().defaultTo(2018)
       table.string('medium').notNullable().defaultTo('')
-      table.string('poster').defaultTo('https://placekitten.com/200/300')
+      table.string('poster').notNullable().defaultTo('https://placekitten.com/200/300')
       table.timestamps(true, true)
     })
   }
