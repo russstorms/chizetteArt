1f27280f67ceaa59c3971444e98393e0790187cf
Russ Storms <rstorms90@gmail.com>
2019-01-01 17:14:55 -0700

    worked on fixed navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,13 +6,13 @@ import './header.css'
 export default class header extends Component {
   render() {
     return (
-      <main className="container">
-        <div className="nav">
-        <span><img className="logo" src={logo} alt="logo"></img><h1 className="chizetteArt">chizetteArt</h1></span>
-        </div>
-        <Button className="backButton btn-flat"><i className="large material-icons icon backIcon">keyboard_arrow_left</i></Button>
-        <Button className="menuButton btn-flat"><i className="large material-icons icon menuIcon">sort</i></Button>
-      </main>
+      <div className="nav">
+      <div className="logoAndTitle">
+      <img className="logo" src={logo} alt="logo"></img><h1 className="chizetteArt"><b>chizette</b>Art</h1>
+      <Button className="menuButton btn-flat"><i className="large material-icons icon menuIcon">sort</i></Button>
+      </div>
+      {/* <Button className="backButton btn-flat"><i className="large material-icons icon backIcon">keyboard_arrow_left</i></Button> */}
+      </div>
     )
   }
 }
