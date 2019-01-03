511bc43d540198541547d179927481372cbd755f
Russ Storms <rstorms90@gmail.com>
2019-01-03 15:18:08 -0700

    sidebar styled

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { Button } from 'react-materialize'
+// import { Button } from 'react-materialize'
+import Sidebar from '../sidebar/sidebar'
 import logo from './chizetteLogo.jpg'
 import './header.css'
-import SideNav from 'react-materialize/lib/SideNav';
-import SideNavItem from 'react-materialize/lib/SideNavItem';
+
 
 export default class header extends Component {
 
@@ -17,23 +17,7 @@ export default class header extends Component {
       <div className="nav">
         <div className="logoAndTitle">
         <img className="logo" src={logo} alt="logo"></img><h1 className="chizetteArt"><b>chizette</b><span onClick={(ev) => this.secretLogin(ev)}>A</span>rt</h1>
-
-      <SideNav
-        trigger={<Button className="menuButton btn-flat"><i className="large material-icons icon menuIcon">sort</i></Button>}
-        options={{ closeonClick: true }}>
-        <div> 
-          <img className="sidebarLogo" src={logo} alt=''></img>
-          <h4 className="sidebarchizetteArt"><b>chizette</b>Art</h4>
-        </div>
-        <br />
-        <SideNavItem href='/chizetteart'>Paintings</SideNavItem>
-        <SideNavItem href='/chizetteart'>Jewelry</SideNavItem>
-        <SideNavItem href='/chizetteart'>Photography</SideNavItem>
-        <SideNavItem divider />
-        <SideNavItem href='#'>Contact Me</SideNavItem>
-        <br />
-        <SideNavItem href='#'>Logout</SideNavItem>
-        </SideNav>
+        <Sidebar />
         </div>
       </div>
     )
---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import SideNav from 'react-materialize/lib/SideNav'
+import SideNavItem from 'react-materialize/lib/SideNavItem'
+import { Button } from 'react-materialize'
+import './sidebar.css'
+
+
+export default class Sidebar extends React.Component {
+
+  render() {
+    return (
+      <SideNav
+        trigger={<Button className="menuButton btn-flat"><i className="large material-icons icon menuIcon">sort</i></Button>}
+        options={{ closeonClick: true }}>
+        <div>
+          <h4 className="sidebarchizetteArt"><b>chizette</b>Art</h4>
+        </div>
+        <br />
+        <SideNavItem href='/chizetteart'>Paintings</SideNavItem>
+        <SideNavItem href='/chizetteart'>Jewelry</SideNavItem>
+        <SideNavItem href='/chizetteart'>Photography</SideNavItem>
+        <SideNavItem divider />
+        <SideNavItem href='#'>Contact Me</SideNavItem>
+        <br />
+        <SideNavItem href='#'>Logout</SideNavItem>
+      </SideNav>
+    )
+  }
+
+}
