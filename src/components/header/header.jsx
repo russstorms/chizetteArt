278efa0fe3d188124337a0a323d24f42219a3337
Russ Storms <rstorms90@gmail.com>
2019-01-03 08:40:42 -0700

    added sidebar but not showing up

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,12 +4,23 @@ import logo from './chizetteLogo.jpg'
 import './header.css'
 
 export default class header extends Component {
+
+  menu = (ev) => {
+    ev.preventDefault()
+    console.log(`Clicked the menu button`)
+  }
+
+  secretLogin = (ev) => {
+    ev.preventDefault()
+    console.log(`Secret Login clicked!`)
+  }
+
   render() {
     return (
       <div className="nav">
       <div className="logoAndTitle">
-      <img className="logo" src={logo} alt="logo"></img><h1 className="chizetteArt"><b>chizette</b>Art</h1>
-      <Button className="menuButton btn-flat"><i className="large material-icons icon menuIcon">sort</i></Button>
+      <img className="logo" src={logo} alt="logo"></img><h1 className="chizetteArt"><b>chizette</b><span onClick={(ev) => this.secretLogin(ev)}>A</span>rt</h1>
+      <Button onClick={(ev) => this.menu(ev)} className="menuButton btn-flat"><i className="large material-icons icon menuIcon">sort</i></Button>
       </div>
       {/* <Button className="backButton btn-flat"><i className="large material-icons icon backIcon">keyboard_arrow_left</i></Button> */}
       </div>
