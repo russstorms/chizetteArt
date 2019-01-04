e3b223b76e0917dccdcb91f2d1aa3f827800ba51
Russ Storms <rstorms90@gmail.com>
2019-01-03 20:34:26 -0700

    grid working but a few bugs in create then edit, parallax working, sidebar working, now sidebar styling branch

--- src/components/parallax/parallax.jsx
+++ src/components/parallax/parallax.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Parallax } from 'react-materialize'
 import './parallax.css'
 
-const Parallax = (props) => {
+const ParallaxImage = (props) => {
   return (
-    <div className="parallax">
+    <div>
+      <Parallax className="parallax" imageSrc="https://images.unsplash.com/photo-1524215358253-2a0988a55a68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"/>
       <h5 className="about"><span className="firstLetter">H</span>i! My name is Chizette and art is my passion. I wish to share my art with the world. Please enjoy!</h5>
     </div>
   )
 }
 
-export default Parallax
+export default ParallaxImage
