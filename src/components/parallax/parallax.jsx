1affafebd516a6d399971a46e9be028eee9af6af
Russ Storms <rstorms90@gmail.com>
2019-01-08 08:02:48 -0700

    margins right but no translate yet

--- src/components/parallax/parallax.jsx
+++ src/components/parallax/parallax.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './parallax.css'
+import { Parallax } from 'react-materialize'
 
 export default class ParallaxImage extends React.Component {
   constructor(props) {
@@ -21,8 +22,9 @@ export default class ParallaxImage extends React.Component {
 
   render () {
     return (
-        <div className="parallax">
-          <img className="img" src={this.props.artList[this.state.counter]} alt="" />
+        <div>
+          {/* <Parallax className="parallax" imgSrc="https://images.unsplash.com/photo-1504221507732-5246c045949b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" /> */}
+          <div className="parallax"><img className="img" src={this.props.artList[this.state.counter]} alt="" /></div>
           <h5 className="about"><span className="firstLetter">H</span>i! My name is Chizette and art is my passion. I wish to share my art with the world. Please enjoy!</h5>
         </div>
     )
