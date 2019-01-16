7f10cad97bebdec86c9adb84cc8412fee51dbb53
Russ Storms <rstorms90@gmail.com>
2019-01-15 21:15:20 -0700

    contact page done

--- src/components/contact-me/contactme.jsx
+++ src/components/contact-me/contactme.jsx
@@ -15,9 +15,15 @@ export default class Contact extends React.Component {
     return (
       <div className="contactContainer">
         <h3 ref={this.myRef} className="contactPage"></h3>
-        <h5 className="contact"><span className="bigLetter">T</span>hank you for visiting, I hope you enjoyed!</h5>
-        <a href="https://www.instagram.com/chizette/" target="blank" className="instagram"><span className="instagramIcon"><i className="fab fa-instagram"></i></span></a>
-        <a href="mailto:chizetteart@gmail.com" target="blank" className="email"><span className="emailIcon"><i className="far fa-envelope"></i></span></a>
+        <img className="contactPic" src="https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/13118936_10153529995520205_5034489750183110230_n.jpg?_nc_cat=106&_nc_ht=scontent.fapa1-1.fna&oh=8ed13a459ab6ae86f3f78dc6fffb5672&oe=5CC35C40" alt="#"></img>
+        <i><h5 className="contact"><span className="bigLetter">C</span>hizette grew up in a pink house on a little island with two artist parents who taught her to draw and paint. She works primarily with gouache and crayon, and with gemstones, natural fibers, seashells, glass, clay, & photography. Currently she lives in the mountains.</h5></i>
+        <br />
+        <br />
+        <i><h5 className="contact">Please email for commissions, collaboration, or questions.</h5></i>
+          <div className="contactIconContainer">
+            <a href="https://www.instagram.com/chizette/" target="blank" className="instagram"><span className="instagramIcon"><i className="fab fa-instagram"></i></span></a>
+            <a href="mailto:chizetteart@gmail.com" target="blank" className="email"><span className="emailIcon"><i className="far fa-envelope"></i></span></a>
+          </div>
         <br />
         <br />
         <h5 className="quote"><i>"Great art picks up where nature ends."</i> ~Chagall</h5>
