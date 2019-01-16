bd386d9add540b735857cc2e51fddca3a0da06d1
Russ Storms <rstorms90@gmail.com>
2019-01-15 20:04:42 -0700

    email added

--- src/components/contact-me/contactme.jsx
+++ src/components/contact-me/contactme.jsx
@@ -17,7 +17,7 @@ export default class Contact extends React.Component {
         <h3 ref={this.myRef} className="contactPage"></h3>
         <h5 className="contact"><span className="bigLetter">T</span>hank you for visiting, I hope you enjoyed!</h5>
         <a href="https://www.instagram.com/chizette/" target="blank" className="instagram"><span className="instagramIcon"><i className="fab fa-instagram"></i></span></a>
-        <a href="/chizetteart" target="blank" className="email"><span className="emailIcon"><i className="far fa-envelope"></i></span></a>
+        <a href="mailto:chizetteart@gmail.com" target="blank" className="email"><span className="emailIcon"><i className="far fa-envelope"></i></span></a>
         <br />
         <br />
         <h5 className="quote"><i>"Great art picks up where nature ends."</i> ~Chagall</h5>
