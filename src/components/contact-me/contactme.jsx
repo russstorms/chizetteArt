0b453d4ff53e1811a516b5566e3fee7b154616e5
Russ Storms <rstorms90@gmail.com>
2019-01-15 18:54:23 -0700

    styled

--- src/components/contact-me/contactme.jsx
+++ src/components/contact-me/contactme.jsx
@@ -17,6 +17,7 @@ export default class Contact extends React.Component {
         <h3 ref={this.myRef} className="contactPage"></h3>
         <h5 className="contact"><span className="bigLetter">T</span>hank you for visiting, I hope you enjoyed!</h5>
         <a href="/chizetteart" target="blank" className="instagram"><span className="instagramIcon"><i className="fab fa-instagram"></i></span><i>instagram</i></a>
+        <a href="/chizetteart" target="blank" className="email"><span className="emailIcon"><i className="far fa-envelope"></i></span><i>email</i></a>
         <br />
         <br />
         <h5 className="quote">"Great art picks up where nature ends." ~Chagall</h5>
