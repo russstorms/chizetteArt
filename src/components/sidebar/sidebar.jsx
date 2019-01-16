5aa25e348cd10ac4b766c1527e6b4bcd7fdfc487
Russ Storms <rstorms90@gmail.com>
2019-01-15 20:20:50 -0700

    added to just root

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -13,7 +13,7 @@ export default class Sidebar extends React.Component {
         trigger={<Button className="menuButton btn-flat"><i className="large material-icons icon menuIcon">sort</i></Button>}
         options={{ closeonClick: true }}>
         <div>
-          <a href="/chizetteart"><h4 className="sidebarchizetteArt"><b>chizette</b>Art</h4></a>
+          <a href="/"><h4 className="sidebarchizetteArt"><b>chizette</b>Art</h4></a>
         </div>
         <br />
           <SideNavItem onClick={this.props.filterArt}><span data-medium="Art" className="iconContainer"><i className="small material-icons icon sidebarIcon">palette</i></span><span className="sideNavTitle">Art</span></SideNavItem>
@@ -22,7 +22,7 @@ export default class Sidebar extends React.Component {
         <SideNavItem divider />
         <br />
         {!this.props.contactMe ? <SideNavItem onClick={this.props.toggleContactMe} ><span className="iconContainer"><span className="sidebarIconContainer"><i className="far fa-user"></i></span></span><span className="sideNavTitle">Contact Me</span></SideNavItem> : <SideNavItem onClick={this.props.toggleContactMe}><div id="toTop"><span className="iconContainer"><span className="sidebarIconContainer"><i className="fas fa-angle-up"></i></span></span>To Top</div></SideNavItem>}
-        {this.props.token ? <SideNavItem href='/chizetteart' onClick={logoutClick} >Logout</SideNavItem>: <h1 className="footerSidebar">chizetteArt</h1>}
+        {this.props.token ? <SideNavItem href='/' onClick={logoutClick} >Logout</SideNavItem>: <h1 className="footerSidebar">chizetteArt</h1>}
       </SideNav>
     )
   }
