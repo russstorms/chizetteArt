3dffa46e93adba5f9bfc37b354a15eb368b05f7c
Russ Storms <rstorms90@gmail.com>
2019-01-24 19:37:18 -0700

    shinyName

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -17,7 +17,7 @@ export default class header extends Component {
       <div className="nav">
         <div className="logoAndTitle">
           <a href="/">
-            <img className="logo" src={logo} alt="logo"></img><h1 className="chizetteArt"><b>chizette</b><span onClick={(ev) => this.secretLogin(ev)}>A</span>rt</h1>
+            <img className="logo" src={logo} alt="logo"></img><h1 className="chizetteArt"><b><span className="c">c</span><span className="h">h</span><span className="i">i</span><span className="z">z</span><span className="e">e</span><span className="t">t</span><span className="t2">t</span><span className="e2">e</span></b><span className="A" onClick={(ev) => this.secretLogin(ev)}>A</span><span className="r">r</span><span className="t3">t</span></h1>
           </a>
             <Sidebar filterArt={this.props.filterArt} toggleContactMe={this.props.toggleContactMe} contactMe={this.props.contactMe} logoutClick={this.props.logoutClick} token={this.props.token} postArt={this.props.postArt} />
             {this.props.token ? <ComposeArt postArt={this.props.postArt} /> : null}
