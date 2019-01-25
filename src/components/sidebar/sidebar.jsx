b63256613c7eb06531beaddd3b0d29aa6bad7d8f
Russ Storms <rstorms90@gmail.com>
2019-01-24 19:52:25 -0700

    took out icons

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -18,12 +18,12 @@ export default class Sidebar extends React.Component {
         </div>
         <br />
           <Badge className="new badge" data-badge-caption="Prints Coming Soon!"></Badge>
-          <SideNavItem onClick={this.props.filterArt}><span data-medium="Art" className="iconContainer"><i className="small material-icons icon sidebarIcon">palette</i></span><span className="sideNavTitle">Art</span></SideNavItem>
-          <SideNavItem onClick={this.props.filterArt}><span data-medium="Jewelry" className="iconContainer"><span className="sidebarIconContainer"><i className="fas fa-ring"></i></span></span><span className="sideNavTitle">Jewelry</span></SideNavItem>
-          <SideNavItem onClick={this.props.filterArt}><span data-medium="Photography" className="iconContainer"><span className="sidebarIconContainer"><i className="fas fa-camera-retro"></i></span></span><span className="sideNavTitle">Photography</span></SideNavItem>
+          <SideNavItem onClick={this.props.filterArt}><span data-medium="Art" className="iconContainer"></span><span className="sideNavTitle">Art</span></SideNavItem>
+          <SideNavItem onClick={this.props.filterArt}><span data-medium="Jewelry" className="iconContainer"><span className="sidebarIconContainer"></span></span><span className="sideNavTitle">Jewelry</span></SideNavItem>
+          <SideNavItem onClick={this.props.filterArt}><span data-medium="Photography" className="iconContainer"><span className="sidebarIconContainer"></span></span><span className="sideNavTitle">Photography</span></SideNavItem>
         <SideNavItem divider />
         <br />
-        {!this.props.contactMe ? <SideNavItem onClick={this.props.toggleContactMe} ><span className="iconContainer"><span className="sidebarIconContainer"><i className="far fa-user"></i></span></span><span className="sideNavTitle">Contact Me</span></SideNavItem> : <SideNavItem onClick={this.props.toggleContactMe}><div id="toTop"><span className="iconContainer"><span className="sidebarIconContainer"><i className="fas fa-angle-up"></i></span></span>To Top</div></SideNavItem>}
+        {!this.props.contactMe ? <SideNavItem onClick={this.props.toggleContactMe} ><span className="iconContainer"><span className="sidebarIconContainer"></span></span><span className="sideNavTitle">Contact Me</span></SideNavItem> : <SideNavItem onClick={this.props.toggleContactMe}><div id="toTop"><span className="iconContainer"><span className="sidebarIconContainer"><i className="fas fa-angle-up"></i></span></span>To Top</div></SideNavItem>}
         {this.props.token ? <SideNavItem href='/' onClick={logoutClick} >Logout</SideNavItem>: <h1 className="footerSidebar">chizetteArt</h1>}
       </SideNav>
     )
