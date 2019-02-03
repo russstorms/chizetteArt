ba414392e011f00f682a521a5bc3a1f39c847856
Russ Storms <rstorms90@gmail.com>
2019-02-03 16:09:40 -0700

    phone

--- src/components/login/loginForm.jsx
+++ src/components/login/loginForm.jsx
@@ -40,7 +40,7 @@ export default class LoginForm extends React.Component {
 				</label>
 				<label htmlFor="icon_lock">
 				<i className="material-icons prefix"></i>
-				<i class="fas fa-user-secret loginIcons"></i><input id="icon_lock" className="inputs" onChange={this.onUsernameChange} name="password" autoComplete="off" placeholder="Secret Code" type="password" />
+				<i className="fas fa-user-secret loginIcons"></i><input id="icon_lock" className="inputs" onChange={this.onUsernameChange} name="password" autoComplete="off" placeholder="Secret Code" type="password" />
 				</label>
 				<button className="goButton">GO</button>
 			</form>
