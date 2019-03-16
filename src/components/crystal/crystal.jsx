62c5567fa798a941fb1ed6ced25ccb2c59807cd3
Russ Storms <rstorms90@gmail.com>
2019-03-15 20:48:18 -0600

    crystals

--- src/components/crystal/crystal.jsx
+++ src/components/crystal/crystal.jsx
@@ -11,6 +11,7 @@ export default class Body extends Component {
           <span></span>
           <span></span>
           <span></span>
+          <span></span>
         </div>
       </div>
     )
