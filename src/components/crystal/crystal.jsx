e51df051221f6a0ebe63bed133be33809306a133
Russ Storms <rstorms90@gmail.com>
2019-03-15 21:38:40 -0600

    foreground crystal

--- src/components/crystal/crystal.jsx
+++ src/components/crystal/crystal.jsx
@@ -12,6 +12,12 @@ export default class Body extends Component {
           <span></span>
           <span></span>
           <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          
         </div>
       </div>
     )
