ec0110a6768532f89ad0cc25e6c8a29582321111
Russ Storms <rstorms90@gmail.com>
2019-03-16 10:56:33 -0600

    merge

--- src/components/crystal/crystal.jsx
+++ src/components/crystal/crystal.jsx
@@ -59,6 +59,7 @@ export default class Body extends Component {
             <span></span>
             <span></span>
             <span></span>
+            <span></span>
           </div>
           <div id="stripes">
             <span></span>
