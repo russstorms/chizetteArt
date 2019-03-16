5f19811726f297847a08fa8c5481b900f21f0ee6
Russ Storms <rstorms90@gmail.com>
2019-03-16 13:37:25 -0600

    lines

--- src/components/crystal/crystal.jsx
+++ src/components/crystal/crystal.jsx
@@ -63,6 +63,8 @@ export default class Body extends Component {
             <span></span>
             <span></span>
             <span></span>
+            <span></span>
+            <span></span>
           </div>
           <div id="stripes">
             <span></span>
