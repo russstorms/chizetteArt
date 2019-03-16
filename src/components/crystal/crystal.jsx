1b1bcb8099c69086be1a74d6403900d95a12a725
Russ Storms <rstorms90@gmail.com>
2019-03-16 07:29:29 -0600

    second finished pillar

--- src/components/crystal/crystal.jsx
+++ src/components/crystal/crystal.jsx
@@ -7,22 +7,24 @@ export default class Body extends Component {
 
   render() {
     return (
-      <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
+      <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" offset={100}>
         <div className="bG">
           <div id="stripes">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+            <span>6</span>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+            <span>11</span>
+            <span>12</span>
+            <span>13</span>
+            <span>14</span>
+            <span>15</span>
           </div>
         </div>
       </ScrollAnimation>
