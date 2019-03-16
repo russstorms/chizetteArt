0b1c41e8190882336fcc1fbf11a5f8cc2b593ad4
Russ Storms <rstorms90@gmail.com>
2019-03-16 07:54:44 -0600

    middle ground done

--- src/components/crystal/crystal.jsx
+++ src/components/crystal/crystal.jsx
@@ -10,21 +10,24 @@ export default class Body extends Component {
       <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" offset={100}>
         <div className="bG">
           <div id="stripes">
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>5</span>
-            <span>6</span>
-            <span>7</span>
-            <span>8</span>
-            <span>9</span>
-            <span>10</span>
-            <span>11</span>
-            <span>12</span>
-            <span>13</span>
-            <span>14</span>
-            <span>15</span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
         </div>
       </ScrollAnimation>
