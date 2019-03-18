5e781ffc4feb3f6a251810c244d087f583449df4
Russ Storms <rstorms90@gmail.com>
2019-03-18 12:07:50 -0600

    grid fixed, still need filter functionality

--- src/components/art-list/artList.jsx
+++ src/components/art-list/artList.jsx
@@ -27,7 +27,7 @@ export default class ArtList extends React.Component {
     let splashPhoto = splashList.splice(0, 3)
 
     if (this.props.filterTerm === '') {
-      return splashArt.map(
+      return [splashArt.map(
         (art, idx) => {
           switch (column) {
             case 'first':
@@ -48,7 +48,7 @@ export default class ArtList extends React.Component {
                   <span className="artT">t</span>
                 </div>
               </ScrollAnimation>
-              <Art art={art} id={idx} artPosters={art} filterTerm={this.props.filterTerm} />
+              <Art art={art} id={idx} artPosters={splashArt} filterTerm={this.props.filterTerm} />
               <ScrollAnimation animateIn="zoomInUp" animateOut="fadeOut">
                 <div onClick={this.splashFilterArt} className="viewArt">
                   <span className="view" data-medium="Art">
@@ -61,25 +61,104 @@ export default class ArtList extends React.Component {
               </ScrollAnimation>
             </div>
         }
+      ),
+      splashJewelry.map(
+        (art, idx) => {
+          switch (column) {
+            case 'first':
+              column = 'second'
+              break
+            case 'second':
+              column = 'third'
+              break
+            default:
+              column = 'first'
+              break
+          }
+            return <div key={idx} className={column}>
+              <ScrollAnimation animateIn="zoomInUp" animateOut="fadeOut">
+                <div className="jewelry">
+                  <span className="jewelryJ">J</span>
+                  <span className="jewelryE">e</span>
+                  <span className="jewelryW">w</span>
+                  <span className="jewelryE2">e</span>
+                  <span className="jewelryL">l</span>
+                  <span className="jewelryR">r</span>
+                  <span className="jewelryY">y</span>
+                </div>
+              </ScrollAnimation>
+              <Art art={art} id={idx} artPosters={splashArt} filterTerm={this.props.filterTerm} />
+              <ScrollAnimation animateIn="zoomInUp" animateOut="fadeOut">
+                <div onClick={this.splashFilterArt} className="viewJewelry">
+                  <span className="view" data-medium="Jewelry">View Jewelry</span>
+                  <span className="arrowIcons">
+                    <i className="small material-icons icon viewAllIcons">chevron_right</i>
+                  </span>
+                </div>
+              </ScrollAnimation>
+            </div>
+        }
+      ),
+      splashPhoto.map(
+        (art, idx) => {
+          switch (column) {
+            case 'first':
+              column = 'second'
+              break
+            case 'second':
+              column = 'third'
+              break
+            default:
+              column = 'first'
+              break
+          }
+            return <div key={idx} className={column}>
+              <ScrollAnimation animateIn="zoomInUp" animateOut="fadeOut">
+                <div className="photography">
+                  <span className="photographyP">P</span>
+                  <span className="photographyH">h</span>
+                  <span className="photographyO">o</span>
+                  <span className="photographyT">t</span>
+                  <span className="photographyO2">o</span>
+                  <span className="photographyG">g</span>
+                  <span className="photographyR">r</span>
+                  <span className="photographyA">a</span>
+                  <span className="photographyP2">p</span>
+                  <span className="photographyH2">h</span>
+                  <span className="photographyY">y</span>
+                </div>
+              </ScrollAnimation>
+              <Art art={art} id={idx} artPosters={splashArt} filterTerm={this.props.filterTerm} />
+              <ScrollAnimation animateIn="zoomInUp" animateOut="fadeOut">
+                <div onClick={this.splashFilterArt} className="viewPhotography">
+                  <span className="view" data-medium="Photography">View Photography</span>
+                  <span className="arrowIcons">
+                    <i className="small material-icons icon viewAllIcons">chevron_right</i>
+                  </span>
+                </div>
+              </ScrollAnimation>
+            </div>
+        }
       )
+    ]
     }
 
-    return this.props.artList.map(
-      (art, idx) => {
-        switch (column) {
-          case 'first':
-            column = 'second'
-            break
-          case 'second':
-            column = 'third'
-            break
-          default:
-            column = 'first'
-            break
-        }
-        return <div key={idx} className={column}><Art art={art} id={idx} artPosters={this.props.artPosters} token={this.props.token} editArt={this.props.editArt} deleteArt={this.props.deleteArt} /></div>
-      }
-    )
+    // return this.props.artList.map(
+    //   (art, idx) => {
+    //     switch (column) {
+    //       case 'first':
+    //         column = 'second'
+    //         break
+    //       case 'second':
+    //         column = 'third'
+    //         break
+    //       default:
+    //         column = 'first'
+    //         break
+    //     }
+    //     return <div key={idx} className={column}><Art art={art} id={idx} artPosters={this.props.artPosters} token={this.props.token} editArt={this.props.editArt} deleteArt={this.props.deleteArt} /></div>
+    //   }
+    // )
   }
 
   render () {
@@ -93,50 +172,6 @@ export default class ArtList extends React.Component {
     )
   }
 }
-
-
-
-{/* <div className="wrapper2">
-
-<ScrollAnimation animateIn="zoomInUp" animateOut="fadeOut">
-  <div className="jewelry">
-    <span className="jewelryJ">J</span>
-    <span className="jewelryE">e</span>
-    <span className="jewelryW">w</span>
-    <span className="jewelryE2">e</span>
-    <span className="jewelryL">l</span>
-    <span className="jewelryR">r</span>
-    <span className="jewelryY">y</span>
-  </div>
-  <div onClick={this.splashFilterArt} className="viewJewelry">
-    <span className="view" data-medium="Jewelry">View Jewelry</span>
-    <span className="arrowIcons">
-      <i className="small material-icons icon viewAllIcons">chevron_right</i>
-    </span>
-  </div>
-</ScrollAnimation>
-<ScrollAnimation animateIn="zoomInUp" animateOut="fadeOut">
-  <div className="photography">
-    <span className="photographyP">P</span>
-    <span className="photographyH">h</span>
-    <span className="photographyO">o</span>
-    <span className="photographyT">t</span>
-    <span className="photographyO2">o</span>
-    <span className="photographyG">g</span>
-    <span className="photographyR">r</span>
-    <span className="photographyA">a</span>
-    <span className="photographyP2">p</span>
-    <span className="photographyH2">h</span>
-    <span className="photographyY">y</span>
-  </div>
-  <div onClick={this.splashFilterArt} className="viewPhotography">
-    <span className="view" data-medium="Photography">View Photography</span>
-    <span className="arrowIcons">
-      <i className="small material-icons icon viewAllIcons">chevron_right</i>
-    </span>
-  </div>
-</ScrollAnimation>
-</div> */}
 
 
 //// FILTER BY MEDIUM \\\\
