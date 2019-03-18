43d6dffc3d91d788090e682ddc05ebf3091346cf
Russ Storms <rstorms90@gmail.com>
2019-03-18 12:10:42 -0600

    grid fixed, still need filter functionality

--- src/components/art-list/artList.jsx
+++ src/components/art-list/artList.jsx
@@ -143,22 +143,22 @@ export default class ArtList extends React.Component {
     ]
     }
 
-    // return this.props.artList.map(
-    //   (art, idx) => {
-    //     switch (column) {
-    //       case 'first':
-    //         column = 'second'
-    //         break
-    //       case 'second':
-    //         column = 'third'
-    //         break
-    //       default:
-    //         column = 'first'
-    //         break
-    //     }
-    //     return <div key={idx} className={column}><Art art={art} id={idx} artPosters={this.props.artPosters} token={this.props.token} editArt={this.props.editArt} deleteArt={this.props.deleteArt} /></div>
-    //   }
-    // )
+    return this.props.artList.map(
+      (art, idx) => {
+        switch (column) {
+          case 'first':
+            column = 'second'
+            break
+          case 'second':
+            column = 'third'
+            break
+          default:
+            column = 'first'
+            break
+        }
+        return <div key={idx} className={column}><Art art={art} id={idx} artPosters={this.props.artPosters} token={this.props.token} editArt={this.props.editArt} deleteArt={this.props.deleteArt} /></div>
+      }
+    )
   }
 
   render () {
