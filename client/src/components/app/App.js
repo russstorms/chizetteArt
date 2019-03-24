237c416c3f6d6aed15dfb837a8e97f858c1bf251
Russ Storms <rstorms90@gmail.com>
2019-03-24 15:44:12 -0600

    separated server from client

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -10,10 +10,9 @@ import Footer from '../footer/footer'
 import { ParallaxProvider } from 'react-scroll-parallax'
 import 'animate.css/animate.min.css'
 
-const API = "http://localhost:3000"
+const API = process.env.REACT_APP_API
 // Switch back on deployment
-// process.env.REACT_APP_API ||
-
+//  || "http://localhost:3000"
 export default class App extends Component {
   constructor(props) {
     super(props)
