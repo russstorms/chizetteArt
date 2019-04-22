5a79c16ba1e7f9f2551f7482948574fdc325d789
Russ Storms <rstorms90@gmail.com>
2019-04-22 16:43:04 -0600

    phone media queries

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -17,7 +17,21 @@ export default class header extends Component {
       <div className="nav">
         <div className="logoAndTitle">
           <a className="anchorForHome" href="/">
-            <img className="logo" src={logo} alt="logo"></img><h1 className="chizetteArt"><b><span className="c">c</span><span className="h">h</span><span className="i">i</span><span className="z">z</span><span className="e">e</span><span className="t">t</span><span className="t2">t</span><span className="e2">e</span></b><span className="A" onClick={(ev) => this.secretLogin(ev)}>A</span><span className="r">r</span><span className="t3">t</span></h1>
+            <img className="logo" src={logo} alt="logo"></img>
+            <h1 className="chizetteArt">
+              <strong className="chizette"><span className="c">c</span>
+                <span className="h">h</span>
+                <span className="i">i</span>
+                <span className="z">z</span>
+                <span className="e">e</span>
+                <span className="t">t</span>
+                <span className="t2">t</span>
+                <span className="e2">e</span>
+              </strong>
+              <span className="A" onClick={(ev) => this.secretLogin(ev)}>A</span>
+              <span className="r">r</span>
+              <span className="t3">t</span>
+            </h1>
           </a>
             <Sidebar filterArt={this.props.filterArt} toggleContactMe={this.props.toggleContactMe} contactMe={this.props.contactMe} logoutClick={this.props.logoutClick} token={this.props.token} postArt={this.props.postArt} />
             {this.props.token ? <ComposeArt postArt={this.props.postArt} /> : null}
