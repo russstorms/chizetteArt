d5de1a059e88b72d1953849f830c9331d11d2020
Russ Storms <rstorms90@gmail.com>
2020-01-10 11:17:56 -0700

    start for mobile first now working up

--- src/components/contact-me/contactme.jsx
+++ src/components/contact-me/contactme.jsx
@@ -20,16 +20,14 @@ export default class Contact extends Component {
           src="https://res.cloudinary.com/chizetteart/image/upload/v1556310776/chizetteArt%20-%20Compressed/contactme.png"
           alt="#">
         </img>
-        <h5 className="contact">
+        <p className="contact">
           <span className="bigLetter">C</span>
             hizette grew up in a pink house on a little island with two artist parents who taught her to draw and paint.
             She works primarily with gouache and crayon, and with gemstones, natural fibers, seashells, glass, clay, & photography.
             Currently, she lives in the mountains.
-        </h5>
-        <br />
-        <br />
+        </p>
         <i>
-          <h5 className="contact">Please email for commissions, collaboration, or questions.</h5>
+          <p className="contact">Please email for commissions, collaboration, or questions.</p>
         </i>
           <div className="contactIconContainer">
             <a
@@ -51,15 +49,12 @@ export default class Contact extends Component {
               </span>
             </a>
           </div>
-        <br />
-        <br />
         <h5 className="quote">
           <i>
             "Great art picks up where nature ends."
           </i>
            ~Chagall
         </h5>
-        <br />
       </div>
     )
   }
