4b285c85d2ce3ad59b6ed01ccc93699492fba0ec
Russ Storms <rstorms90@gmail.com>
2020-01-21 16:26:38 -0700

    cleanup

--- src/components/parallax/parallax.jsx
+++ src/components/parallax/parallax.jsx
@@ -35,7 +35,6 @@ export default class ParallaxImage extends React.Component {
       "https://res.cloudinary.com/chizetteart/image/upload/c_scale,w_1500/v1552434629/chizetteArt%20-%20Compressed/aragonite_necklace.jpg",
       "https://res.cloudinary.com/chizetteart/image/upload/c_scale,w_1300/v1552434635/chizetteArt%20-%20Compressed/Citrine%20%2B%20Amethyst%20Necklace.jpg",
       "https://res.cloudinary.com/chizetteart/image/upload/c_scale,w_1500/v1552434634/chizetteArt%20-%20Compressed/geode_slice.jpg",
-      "https://res.cloudinary.com/chizetteart/image/upload/c_scale,w_1500/v1552434665/chizetteArt%20-%20Compressed/hale'iwa%20evening.jpg",
       "https://res.cloudinary.com/chizetteart/image/upload/v1552434640/chizetteArt%20-%20Compressed/gold%2Bblue.jpg",
       "https://res.cloudinary.com/chizetteart/image/upload/c_scale,w_1500/v1552434663/chizetteArt%20-%20Compressed/constellations.jpg",
       "https://res.cloudinary.com/chizetteart/image/upload/c_scale,w_1300/v1552434670/chizetteArt%20-%20Compressed/sunset_beach.jpg"
@@ -46,14 +45,13 @@ export default class ParallaxImage extends React.Component {
           layers={[
             {
               image: `${parallaxPosters[this.state.counter]}`,
-              amount: 0.5,
-              slowerScrollRate: false,
+              amount: 0.7,
             },
-              
           ]}
           style={{
             height: '85vh',
-          }}>
+          }}
+        >
         </ParallaxBanner>
         <Parallax
           offsetYMax={300}
