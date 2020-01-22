89414b26ad2075139451a7e6eb41c8a41cab8380
Russ Storms <rstorms90@gmail.com>
2020-01-22 13:54:54 -0700

    up to date

---
+++ src/components/Login-Form/LoginForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { TextField } from "@material-ui/core"
+import './styles/LoginForm.css'
+
+export default function LoginForm({ loginSubmit }) {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+  return (
+    <div className="LoginForm">
+			<form 
+				onSubmit={e => {
+					e.preventDefault()
+					loginSubmit({ username, password })
+					setUsername('')
+					setPassword('')
+				}}
+			>
+        <TextField
+					variant="outlined"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          margin="normal"
+          label="Username"
+					fullWidth
+					required
+        />
+				<TextField
+					type="password"
+					variant="outlined"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          margin="normal"
+          label="Secret Code"
+					fullWidth
+					required
+        />
+				<button
+					className="commonBtn"
+					type="submit">
+						Go
+				</button>
+      </form>
+    </div>
+  )
+}
