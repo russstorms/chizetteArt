30241ad4cc7a48ad1fed68d9d6dacd23a2e6aadc
Russ Storms <rstorms90@gmail.com>
2020-01-31 12:50:06 -0700

    function to spit out spans

--- src/components/crystal/Crystal.jsx
+++ src/components/crystal/Crystal.jsx
@@ -6,89 +6,27 @@ import './styles/Crystal.css'
 import './styles/CrystalDark.css'
 import './styles/CrystalForeGround.css'
 
+const spanCreator = (num) => {
+  let spans = [];
+  for (let i = 0; i < num; i++) {
+    spans.push(<span key={i}></span>);
+  }
+  return spans;
+}
+
 export default function Crystal() {
 
   return (
     <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" offset={100}>
       <div className="Crystal">
         <div id="foreGround">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          {spanCreator(40)}
         </div>
         <div id="darkStripes">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          {spanCreator(14)}
         </div>
         <div id="stripes">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          {spanCreator(19)}
         </div>
       </div>
     </ScrollAnimation>
